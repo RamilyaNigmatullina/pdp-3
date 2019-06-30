5b4fe8605dd8d3495b06ea10e95d0b2746e49941
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2019-06-30 12:28:31 +0300

    Fix post policy

--- app/policies/admin/post_policy.rb
+++ app/policies/admin/post_policy.rb
@@ -1,10 +1,14 @@
 module Admin
   class PostPolicy < ApplicationPolicy
-    def new?
+    def create?
       user&.admin?
     end
 
-    def edit?
+    def update?
+      user&.admin? && record.user == user
+    end
+
+    def destroy?
       user&.admin? && record.user == user
     end
   end
--- spec/policies/admin/post_policy_spec.rb
+++ spec/policies/admin/post_policy_spec.rb
@@ -4,8 +4,8 @@ describe Admin::PostPolicy do
   let(:policy) { described_class.new(post, user: user) }
   let(:post) { build_stubbed :post }
 
-  describe "#new" do
-    subject { policy.apply(:new?) }
+  describe "#create" do
+    subject { policy.apply(:create?) }
 
     context "when user is admin" do
       let(:user) { create :admin }
@@ -26,8 +26,25 @@ describe Admin::PostPolicy do
     end
   end
 
-  describe "#edit" do
-    subject { policy.apply(:edit?) }
+  describe "#update" do
+    subject { policy.apply(:update?) }
+
+    context "when user is author of the post" do
+      let(:post) { build_stubbed :post, user: user }
+      let(:user) { create :admin }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when user is not admin of the post" do
+      let(:user) { create :admin }
+
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe "#destroy" do
+    subject { policy.apply(:destroy?) }
 
     context "when user is author of the post" do
       let(:post) { build_stubbed :post, user: user }
