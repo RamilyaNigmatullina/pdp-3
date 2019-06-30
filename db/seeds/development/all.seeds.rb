0a2222279e10599a61aaebb9d271add36e261884
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2019-06-30 12:22:29 +0300

    Fix seeds

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -1,10 +1,10 @@
-FactoryGirl.create(:super_admin, email: "super_admin@example.com")
+FactoryBot.create(:super_admin, email: "super_admin@example.com")
 FactoryBot.create(:admin, email: "admin@example.com")
 
-user = FactoryGirl.create(:user, email: "user@example.com")
-second_user = FactoryGirl.create(:user, email: "second_user@example.com")
-third_user = FactoryGirl.create(:user, email: "third_user@example.com")
+user = FactoryBot.create(:user, email: "user@example.com")
+second_user = FactoryBot.create(:user, email: "second_user@example.com")
+third_user = FactoryBot.create(:user, email: "third_user@example.com")
 
-FactoryGirl.create_list(:post, 4, user: user)
-FactoryGirl.create_list(:post, 6, :published, user: second_user)
-FactoryGirl.create_list(:post, 2, :rejected, user: third_user)
+FactoryBot.create_list(:post, 4, user: user)
+FactoryBot.create_list(:post, 6, :published, user: second_user)
+FactoryBot.create_list(:post, 2, :rejected, user: third_user)
