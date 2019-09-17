fb60c0f00b058452dfebce0bdfafe3e7f794a316
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2019-09-18 00:23:49 +0300

    Revert "Fix (#13)"
    This reverts commit 43a6877d013921b42aa5e45529e380d869dd1b78.

--- spec/features/admin/statistics/post_ration_spec.rb
+++ spec/features/admin/statistics/post_ration_spec.rb
@@ -1,23 +1,23 @@
-# require "rails_helper"
-#
-# feature "Show Post Ratio Statistics", :js do
-#   include_context :when_super_admin_signed_in
-#   include_context :when_time_is_frozen
-#
-#   before do
-#     create_list :post, 2, :published, created_at: 3.days.ago
-#     create :post, :rejected, created_at: 5.days.ago
-#   end
-#
-#   scenario "Admin sees post ratio statistics" do
-#     visit posts_ratio_admin_statistics_path
-#
-#     fill_in "From", with: 3.days.ago
-#     fill_in "Until", with: 1.day.ago
-#
-#     click_on "Show"
-#
-#     expect(page).to have_content("Posts created from 02 Sep 2019 until 04 Sep 2019.")
-#     expect(page).not_to have_content("No posts")
-#   end
-# end
+require "rails_helper"
+
+feature "Show Post Ratio Statistics", :js do
+  include_context :when_super_admin_signed_in
+  include_context :when_time_is_frozen
+
+  before do
+    create_list :post, 2, :published, created_at: 3.days.ago
+    create :post, :rejected, created_at: 5.days.ago
+  end
+
+  scenario "Admin sees post ratio statistics" do
+    visit posts_ratio_admin_statistics_path
+
+    fill_in "From", with: 3.days.ago
+    fill_in "Until", with: 1.day.ago
+
+    click_on "Show"
+
+    expect(page).to have_content("Posts created from 02 Sep 2019 until 04 Sep 2019.")
+    expect(page).not_to have_content("No posts")
+  end
+end
